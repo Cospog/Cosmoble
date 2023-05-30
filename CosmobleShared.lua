da694f39919028e501520dd958f8e31d6a30ec26
Cospog <105358211+Cospog@users.noreply.github.com>
2023-05-30 18:20:44 +0300

    Create CosmobleShared.lua

---
+++ CosmobleShared.lua
@@ -0,0 +1,28 @@
+--ReplicatedStorage
+local Cosmoble = {activeTables = {}}
+
+script.CosmobleEvent.OnClientEvent:Connect(function(cosmobleName, tbl, key, value)
+	if Cosmoble.activeTables[cosmobleName] then
+		for i,v in pairs(Cosmoble.activeTables[cosmobleName]) do
+			v(tbl, key, value)
+		end
+	end
+end)
+
+function Cosmoble:connectCosmoble(cosmobleName: string, connectionName: string, func)
+	if not self.activeTables[cosmobleName] then
+		self.activeTables[cosmobleName] = {}
+		self.activeTables[cosmobleName][connectionName] = func
+	elseif not self.activeTables[cosmobleName][connectionName] then
+		self.activeTables[cosmobleName][connectionName] = func
+	end
+end
+
+function Cosmoble:disconnectCosmoble(cosmobleName: string, connectionName: string)
+	local cosmobleTblData = self.activeTables[cosmobleName]
+	if cosmobleTblData and cosmobleTblData[connectionName] then
+		cosmobleTblData[connectionName] = nil
+	end
+end
+
+return Cosmoble 
