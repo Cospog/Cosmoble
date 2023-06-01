07fb8ee01158891651fb205b9463235c8a6902a5
Cospog <105358211+Cospog@users.noreply.github.com>
2023-06-01 14:09:00 +0300

    Update CosmobleShared.lua

--- CosmobleShared.lua
+++ CosmobleShared.lua
@@ -2,25 +2,25 @@
 -- Create a table named Cosmoble with an empty table named activeTables inside it
 local Cosmoble = {activeTables = {}}
 
-script.CosmobleEvent.OnClientEvent:Connect(function(cosmobleName, tbl, key, value)
+script.CosmobleEvent.OnClientEvent:Connect(function(cosmobleName, key, value)
 	if Cosmoble.activeTables[cosmobleName] then
 		for i,v in pairs(Cosmoble.activeTables[cosmobleName]) do
-			v(tbl, key, value)
+			v(key, value)
 		end
 	end
 end)
 
 function Cosmoble:connectCosmoble(cosmobleName: string, connectionName: string, func)
-	if not self.activeTables[cosmobleName] then
-		self.activeTables[cosmobleName] = {}
-		self.activeTables[cosmobleName][connectionName] = func
-	elseif not self.activeTables[cosmobleName][connectionName] then
-		self.activeTables[cosmobleName][connectionName] = func
+	if not Cosmoble.activeTables[cosmobleName] then
+		Cosmoble.activeTables[cosmobleName] = {}
+		Cosmoble.activeTables[cosmobleName][connectionName] = func
+	elseif not Cosmoble.activeTables[cosmobleName][connectionName] then
+		Cosmoble.activeTables[cosmobleName][connectionName] = func
 	end
 end
 
 function Cosmoble:disconnectCosmoble(cosmobleName: string, connectionName: string)
-	local cosmobleTblData = self.activeTables[cosmobleName]
+	local cosmobleTblData = Cosmoble.activeTables[cosmobleName]
 	if cosmobleTblData and cosmobleTblData[connectionName] then
 		cosmobleTblData[connectionName] = nil
 	end
