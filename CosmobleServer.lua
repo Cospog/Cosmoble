3c871b46b7f82db3a502006e7e6676905236016a
Cospog <105358211+Cospog@users.noreply.github.com>
2023-08-16 21:28:51 +0300

    Update CosmobleServer.lua
    revamp, improved, easier

--- CosmobleServer.lua
+++ CosmobleServer.lua
@@ -1,26 +1,11 @@
 --ServerScriptService
 local Cosmoble = {activeTables = {}}
 
-
-local function tblToString(tbl)
-	local str = "{"
-	for i, v in pairs(tbl) do
-		if type(v) == "table" then
-			str = str..tostring(i).." = "..tblToString(v)..", "
-		else
-			str = str..tostring(i).." = "..tostring(v)..", "
-		end
-	end
-	if str ~= "{" then
-		str = str:sub(1, -3)
-	end
-	return str.."}"
-end
-
-function Cosmoble.new(name: any, tbl: {}, player)
+function Cosmoble.new(name: any, tbl: {}, plr: Player?)
 	if not Cosmoble.activeTables[name] then
 		Cosmoble.activeTables[name] = {}
-		local proxyTable = {}
+		local proxyTable = newproxy(true)
+		local metaProxyTable = getmetatable(proxyTable)
 		local function sendChanges(key, val)
 			if Cosmoble.activeTables[name] ~= {} then
 				for i,v in pairs(Cosmoble.activeTables[name]) do
@@ -28,79 +13,36 @@ function Cosmoble.new(name: any, tbl: {}, player)
 						v(key, val)
 					end
 				end
-				if player then
-					game.ReplicatedStorage.CosmobleShared.CosmobleEvent:FireClient(player, name, key, val)
+				if plr then
+					game.ReplicatedStorage.CosmobleShared.CosmobleEvent:FireClient(plr, name, key, val)
 				else
 					game.ReplicatedStorage.CosmobleShared.CosmobleEvent:FireAllClients(name, key, val)
 				end
 			end
+		end	
+		local proxyMethods = {
+			insert = function(value: any) return metaProxyTable.__newindex(proxyTable, #proxyTable+1, value) end,
+			remove = function(pos: number) return metaProxyTable.__newindex(proxyTable, pos, nil) end,
+			find = function(needle: any, init: number) return table.find(tbl, needle, init) end,
+			concat = function(sep: string, i: number, j: number) return table.concat(tbl, sep, i, j) end,
+			clone = function() return table.clone(tbl) end,
+			move = function(a: number, b: number, t: number, dst: {}) return table.move(tbl, a, b, t, dst) end,
+			original = function() return tbl end,
+			inpairs = function(func) for i,v in pairs(tbl) do func(i,v) end end
+		}
+		metaProxyTable.__index = function(s,k)
+			local val = tbl[k] or proxyMethods[k]
+			local plrId = ""
+			if plr and plr.UserId then plrId = plr.UserId.."_" end
+			if type(val) == "table" then return Cosmoble.new(plrId.."auto_"..tostring(k), val, plr) end
+			return val
 		end
-		proxyTable.clear = function() return getmetatable(proxyTable).__newindex(proxyTable, nil) end
-		proxyTable.insert = function(first: any, value: any)
-			if value then
-				return getmetatable(proxyTable).__newindex(proxyTable, value, "index")
-			else
-				return getmetatable(proxyTable).__newindex(proxyTable, first)
-			end
+		metaProxyTable.__newindex = function(self,k,v)
+			sendChanges(k,v)
+			tbl[k] = v
 		end
-		proxyTable.remove = function(pos: number) return getmetatable(proxyTable).__newindex(proxyTable, pos, "delete") end
-		proxyTable.find = function(needle: any, init: number)return table.find(tbl, needle, init) end
-		proxyTable.concat = function(sep: string, i: number, j: number) return table.concat(tbl, sep, i, j) end
-		proxyTable.clone = function() return table.clone(tbl) end
-		proxyTable.move = function(a: number, b: number, t: number, dst: {}) return table.move(tbl, a, b, t, dst) end
-		proxyTable.sort = function(comp)
-			for i = 1, #proxyTable do
-				for i2 = i+1, #proxyTable do
-					if comp(proxyTable[i2], proxyTable[i]) then
-						proxyTable.__newindex(proxyTable, i, proxyTable[i2])
-						proxyTable.__newindex(proxyTable, i2, proxyTable[i])
-					end
-				end
-			end
-		end
-		proxyTable.original = function() return tbl end
-		setmetatable(proxyTable, {
-			__index = function(t, k)
-				local val = tbl[k]
-				if type(val) == "table" then
-					if player then
-						val = Cosmoble.new(player.UserId.."_auto_"..tostring(k), val, player)
-					else
-						val = Cosmoble.new("auto_"..tostring(k), val)
-					end
-				end
-				return val
-			end,
-			__newindex = function(t, k, v)
-				if v == "insert" then
-					table.insert(tbl, 1, k)
-					sendChanges(tbl, k)
-				end
-				if v == "delete" then
-					table.remove(tbl, k)
-					sendChanges(k, nil)
-				elseif not k then
-					tbl = {}
-					sendChanges(tbl)
-				elseif not v then
-					table.insert(tbl, k)
-					sendChanges(tbl, k)
-				else
-					tbl[k] = v
-					sendChanges(k, v)
-				end
-			end,
-			__tostring = function() return tblToString(tbl) end
-		})
-		for i,v in pairs(tbl) do
-			if type(v) == "table" then
-				if player then
-					v = Cosmoble.new(player.UserId.."_auto_"..tostring(i), v, player)
-				else
-					v = Cosmoble.new("auto_"..tostring(i), v)
-				end
-			end
-		end
+		metaProxyTable.__iter = function() return next, tbl end
+		metaProxyTable.__len = function() return #tbl end
 		Cosmoble.activeTables[name].proxyTableAuto = proxyTable
 		return proxyTable
 	else
